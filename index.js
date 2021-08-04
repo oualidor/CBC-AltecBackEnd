84670671cfd141af2fe29e0f38d5c0cc3f86c326
oualidor <walid.khial@gmail.com>
2021-08-04 15:45:45 +0100

    Simple Express and cors server running

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 8080;
+
+
+const app = express();
+app.use(cors());
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }))
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server  running on  ${PORT}.`)
+});
