e8f74bf64658418bd287b24461096387f1817d9b
oualidor <walid.khial@gmail.com>
2021-08-05 12:49:50 +0100

    Client Schema added

---
+++ Schemas/Client.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const db = require('../Apis/sqConnection')
+
+
+const Client = db.define('Clients', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    mail: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    phone: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    fullName: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+    },
+    stat: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+module.exports = Client;
