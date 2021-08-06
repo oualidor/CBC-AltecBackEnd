25db1e9b3994e08aa23c5b272ce9ef48ac0b48e4
oualidor <walid.khial@gmail.com>
2021-08-06 11:51:17 +0100

    Partner Schema added

---
+++ Schemas/Partner.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize');
+const db = require('../Apis/sqConnection')
+
+
+const Partner = db.define('Clients', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    mail: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    phone: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    fullName: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+    },
+    stat: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    wilayaId : {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    communeId : {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    x: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    y: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    }
+});
+
+module.exports = Partner;
