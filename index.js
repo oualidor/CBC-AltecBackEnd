9bb7ea25f455759da64a806b6997bff0c51fb3d1
oualidor <walid.khial@gmail.com>
2021-08-06 14:26:16 +0100

    yit logger Added (simple version)

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const yitLogger  = require('./Apis/yitLogger')
+
 
 const PORT = process.env.PORT || 8080;
 
 
 const app = express();
+
+
+
+
 app.use(cors());
 
-/* Parse URL-encoded bodies (as sent by HTML forms)*/app.use(express.urlencoded({ extended: true }))
-// Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.json());
 
 
