48f6b6beb20c0eb73b362866ef1ad523ddbcb5c6
oualidor <walid.khial@gmail.com>
2021-09-24 20:17:08 +0100

    correct realtime info end point server

--- InternEndPoints/StationRouters.js
+++ InternEndPoints/StationRouters.js
@@ -53,7 +53,8 @@ const  AdminStationRouters = {
             }
         }),
         getRealTimeInfo: router.get('/getRealTimeInfo/:id', async (req, res) => {
-            let requestAddress = TCP_SERVER+'queryPowerBankInfo'
+            let {boxId} = req.params
+            let requestAddress = TCP_SERVER+'Station/QueryInfo/'+boxId
             try {
                 const request  = await axios({url: requestAddress, method: "get", responseType: 'json'})
                 res.send(request.data)
