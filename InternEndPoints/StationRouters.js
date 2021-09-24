5702d9a225554fe220a53c7c31d3d6561b57994f
oualidor <walid.khial@gmail.com>
2021-09-24 20:50:14 +0100

    testing endpoint url, dont fetch here

--- InternEndPoints/StationRouters.js
+++ InternEndPoints/StationRouters.js
@@ -53,8 +53,8 @@ const  AdminStationRouters = {
             }
         }),
         getRealTimeInfo: router.get('/getRealTimeInfo/:id', async (req, res) => {
-            let {boxId} = req.params
-            let requestAddress = TCP_SERVER+'Station/QueryInfo/'+boxId
+            let {id} = req.params
+            let requestAddress = TCP_SERVER+'Station/QueryInfo/'+id
             console.log(requestAddress)
             try {
                 const request  = await axios({url: requestAddress, method: "get", responseType: 'json'})
