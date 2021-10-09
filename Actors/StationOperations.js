1f1530c139a4c282f5deac3360667d485306598f
oualidor <walid.khial@gmail.com>
2021-10-09 15:58:21 +0100

    debugging

--- Actors/StationOperations.js
+++ Actors/StationOperations.js
@@ -29,6 +29,7 @@ const StationGlobalRouters = {
             const preparedData = UpdateData(data)
             let currentActor = await CurrentActor.findByPk(id);
             if(currentActor != null){
+                console.log(preparedData)
                 await currentActor.update(preparedData);
                 return  GlOpResult(true, "Station updated")
             }else{
