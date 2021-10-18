a9ccb677cbe71e1fddf40c65b6aa205528a0277d
oualidoor <walid.khial@gmail.com>
2021-10-18 10:18:19 +0100

    Debugging

--- InternEndPoints/Admin/AdminStationRouters.js
+++ InternEndPoints/Admin/AdminStationRouters.js
@@ -5,10 +5,9 @@ const StationOperations = require("../../Actors/StationOperations");
 const AnswerHttpRequest = require("../../Structures/AnswerHttpRequest");
 const Station = require("../../Schemas/Station");
 const _EndPoints = require("../../InternEndPoints/Admin/_EndPoints");
-const _StationEndPoints = new _EndPoints(Station, StationOperations)
 const AdminStationRouters = express.Router();
 
-AdminStationRouters.use('/', _StationEndPoints),
+AdminStationRouters.use('/', new _EndPoints(Station, StationOperations)),
 
 AdminStationRouters.post('/update/:id', async (req, res) => {
     let {id} = req.params
