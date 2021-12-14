723dbce028fe2f94c1d97dced6ce27ee447850ff
oualidor <walid.khial@gmail.com>
2021-12-14 20:55:41 +0100

    StationMiddleware  added

---
+++ Apis/Middlewares/StationMiddleware.js
@@ -0,0 +1,42 @@
+const AnswerHttpRequest = require("../../Structures/AnswerHttpRequest");
+const ClientOperations = require("../../Actors/ClientOperations");
+const StationOperations = require("../../Actors/StationOperations");
+const StationMiddleware = {
+
+    byPassStat :  (station, req, res, next) => {
+        try {
+            switch (station.stat){
+                case 0:
+                    next()
+                    break;
+                case 1:
+                    AnswerHttpRequest.wrong(res, "Station is frozen by administration")
+                    break;
+                default:
+                    AnswerHttpRequest.wrong(res, "Operation failed")
+
+            }
+        }
+        catch (error){
+            AnswerHttpRequest.wrong(res, "Operation failed")
+        }
+    },
+
+    validateExistence :  async (stationPublicId, req, res, next) => {
+        try {
+            let stationFindOperation = await StationOperations.getOneByPublicId(stationPublicId)
+            if(stationFindOperation.finalResult){
+                let station = stationFindOperation.result
+                req.station = station
+                next()
+            } else {
+                AnswerHttpRequest.wrong(res, stationFindOperation.error)
+            }
+        } catch (error) {
+            AnswerHttpRequest.wrong(res, "Operation failed")
+        }
+    }
+}
+
+
+module.exports = StationMiddleware
--- InternEndPoints/Client/ClientStationRouters.js
+++ InternEndPoints/Client/ClientStationRouters.js
@@ -12,9 +12,24 @@ const SettingOperations = require("../../Actors/SettingOperations");
 const SettingsMiddleware = require("../../Apis/Middlewares/SettingsMiddleware");
 const YitLogger = require("../../Apis/YitLogger");
 const ErrorLog = require("../../Structures/ErrorLog");
+const StationMiddleware = require("../../Apis/Middlewares/StationMiddleware");
 const  ClientStationRouters = express.Router()
 
-ClientStationRouters.get('/getRealTimeInfo/:id', async (req, res) => {
+
+ClientStationRouters.use(
+    (req, res, next) =>{
+        let stationPublicId = req.body.StationId
+        StationMiddleware.validateExistence(stationPublicId, req, res, next).then()
+    }
+)
+
+ClientStationRouters.use(
+    (req, res, next) =>{
+        StationMiddleware.byPassStat(req['station'], req, res, next)
+    }
+)
+
+ClientStationRouters.get('/getRealTimeInfo/', async (req, res) => {
     await StationOperations.getRealTimeInfo(req, res)
 }),
 
@@ -31,58 +46,52 @@ ClientStationRouters.post(
                 let stationPublicId = req.body.StationId
                 let clientFindOperation = await ClientGlobalOperations.findByPk(clientId)
                 if(clientFindOperation.finalResult){
-                    let stationFindOperation = await StationOperations.getOneByPublicId(stationPublicId)
-                    if(stationFindOperation.finalResult){
-                        let currentStation = stationFindOperation.result
-                        let currentClient = clientFindOperation.result
-                        let currentBalance  = parseInt(currentClient.Wallet.balance)
-                        let getFeesOp =  await SettingOperations.getOne("rentFees")
-                        // let rentFees = getFeesOp.result.dataValue
-                        let rentFees = currentStation.price
-                        if(currentBalance >= rentFees){
-                            let newBalance = currentBalance - rentFees
-                            let walletUpdateOperation = await ClientWalletGlobalOperations.update(currentClient['Wallet'].id, {balance: newBalance})
-                            if(walletUpdateOperation.finalResult){
-                                let rentResult = await StationOperations.rentPowerBank(stationPublicId)
-                                if(rentResult.finalResult === true){
-                                    let rentTransactionsResults = await TransactionOperations.create(
-                                        TransactionTypes.station.rent,
-                                        [
-                                            {dataTitle: "stationId", dataValue: currentStation.systemId},
-                                            {dataTitle: "clientId", dataValue: clientId},
-                                            {dataTitle: "powerBankId", dataValue: rentResult.data.powerBankId},
-                                        ]
+                    let currentStation = req.station
+                    let currentClient = clientFindOperation.result
+                    let currentBalance  = parseInt(currentClient['Wallet'].balance)
+                    //let getFeesOp =  await SettingOperations.getOne("rentFees")
+                    // let rentFees = getFeesOp.result.dataValue
+                    let rentFees = currentStation.price
+                    if(currentBalance >= rentFees){
+                        let newBalance = currentBalance - rentFees
+                        let walletUpdateOperation = await ClientWalletGlobalOperations.update(currentClient['Wallet'].id, {balance: newBalance})
+                        if(walletUpdateOperation.finalResult){
+                            let rentResult = await StationOperations.rentPowerBank(stationPublicId)
+                            if(rentResult.finalResult === true){
+                                let rentTransactionsResults = await TransactionOperations.create(
+                                    TransactionTypes.station.rent,
+                                    [
+                                        {dataTitle: "stationId", dataValue: currentStation.systemId},
+                                        {dataTitle: "clientId", dataValue: clientId},
+                                        {dataTitle: "powerBankId", dataValue: rentResult.data.powerBankId},
+                                    ]
+                                )
+                                if(rentTransactionsResults.finalResult === false){
+                                    let logEntry = ErrorLog.Transaction.rent(
+                                        currentStation.id,
+                                        rentResult.data.powerBankId,
+                                        clientId,
+                                        "Could not write transaction to DB after success rent operation"
                                     )
-                                    if(rentTransactionsResults.finalResult === false){
-                                        let logEntry = ErrorLog.Transaction.rent(
-                                            stationFindOperation.result.id,
-                                            rentResult.data.powerBankId,
-                                            clientId,
-                                            "Could not write transaction to DB after success rent operation"
-                                        )
-                                        YitLogger.error({ message: logEntry})
-                                    }
-
-                                    client.update({type: 0})
-                                    AnswerHttpRequest.done(res, "Power bank rented successfully")
-                                }
-                                else {
-                                    newBalance = newBalance + rentFees
-                                    await ClientWalletGlobalOperations.update(currentClient.Wallet.id, {balance: newBalance})
-                                    let logEntry = ErrorLog.WalletUpdate.reFund(clientId, currentBalance, "wallet refund failed after charge for rent")
                                     YitLogger.error({ message: logEntry})
-                                    AnswerHttpRequest.wrong(res, rentResult.error)
                                 }
-                            }else {
-                                YitLogger.error({ message: ErrorLog.WalletUpdate.rent(clientId, "charge")})
-                                AnswerHttpRequest.wrong(res, walletUpdateOperation.error)
+
+                                client.update({type: 0})
+                                AnswerHttpRequest.done(res, "Power bank rented successfully")
+                            }
+                            else {
+                                newBalance = newBalance + rentFees
+                                await ClientWalletGlobalOperations.update(currentClient['Wallet'].id, {balance: newBalance})
+                                let logEntry = ErrorLog.WalletUpdate.reFund(clientId, currentBalance, "wallet refund failed after charge for rent")
+                                YitLogger.error({ message: logEntry})
+                                AnswerHttpRequest.wrong(res, rentResult.error)
                             }
                         }else {
-                            AnswerHttpRequest.wrong(res, "Insufficient balance")
+                            YitLogger.error({ message: ErrorLog.WalletUpdate.rent(clientId, "charge")})
+                            AnswerHttpRequest.wrong(res, walletUpdateOperation.error)
                         }
-                    }
-                    else {
-                        AnswerHttpRequest.wrong(res, "Unknown Station")
+                    }else {
+                        AnswerHttpRequest.wrong(res, "Insufficient balance")
                     }
                 }else {
                     AnswerHttpRequest.wrong(res, clientFindOperation.error)
