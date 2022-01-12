22b690eb8343a2350ada73af292ffb282c1615be
oualidor <walid.khial@gmail.com>
2022-01-12 18:35:58 +0100

    Partner transaction stats return sorted by days as an array

--- InternEndPoints/Partner/PartnerStatsRouters.js
+++ InternEndPoints/Partner/PartnerStatsRouters.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AnswerHttpRequest = require("../../Structures/AnswerHttpRequest");
 const Transaction = require("../../Schemas/Transaction");
 const Station = require("../../Schemas/Station");
+const Partner = require("../../Schemas/Partner");
 const TransactionMetaData = require("../../Schemas/TransactionMetaData");
 
 const {Op, fn, col} = require("sequelize");
@@ -9,86 +10,106 @@ const  PartnerStatsRouter = express.Router()
 
 
 
+// PartnerStatsRouter.get('/transaction/:operation/:from/:to', async (req, res) => {
+//     let {operation, from, to}  =req.params
+//
+//
+//     try{
+//         let options = {
+//             attributes: [
+//                 [fn('COUNT', col('id')), 'total']
+//             ],
+//         }
+//         let transactions  = await Transaction.findAndCountAll({
+//             where: {
+//                 'operation': operation,
+//                 createdAt: {
+//                     [Op.between]: [new Date(from), new Date(to)],
+//                 }},
+//             include: [{
+//                 model: TransactionMetaData,
+//                 as: 'MetaData',
+//             }]
+//         });
+//         AnswerHttpRequest.done(res, transactions.rows.length)
+//     }
+//     catch (error){
+//         console.log(error)
+//         AnswerHttpRequest.wrong(res, "Request failed")
+//     }
+//
+//
+//
+//
+// }),
+
+
+
+
 PartnerStatsRouter.get('/transaction/:operation/:from/:to', async (req, res) => {
     let {operation, from, to}  =req.params
-
-
     try{
-        let options = {
-            attributes: [
-                [fn('COUNT', col('id')), 'total']
-            ],
+        let partner = await Partner.findByPk(req.body.partner.id, {
+            include : [
+                {
+                    model: Station,
+                    as: "Stations"
+                }
+            ]
+        })
+        if(partner === null){
+            AnswerHttpRequest.wrong(res, "No stations affected to this partner, no stats")
+
         }
-        let transactions  = await Transaction.findAndCountAll({
-            where: {
-                'operation': operation,
-                createdAt: {
-                    [Op.between]: [new Date(from), new Date(to)],
-                }},
-            include: [{
-                model: TransactionMetaData,
-                as: 'MetaData',
-            }]
-        });
-        AnswerHttpRequest.done(res, transactions.rows.length)
+        else {
+            try{
+                let partnerStations = []
+                for (let station of partner['Stations']){
+                    partnerStations.push(station.systemId.toString())
+                }
+                let options = {
+                    attributes: [
+                        [fn('COUNT', col('id')), 'total']
+                    ],
+                    where : {
+                        operation: operation,
+                        createdAt: {
+                            [Op.between]: [new Date(from), new Date(to)],
+                        }}
+                }
+                let transactions  = await Transaction.findAndCountAll({
+                    raw: true,
+                    where: {'operation': operation},
+                    include: [{
+                        model: TransactionMetaData,
+                        as: 'MetaData',
+                        where: {
+                            [Op.and]: [
+                                {"dataTitle": "stationId"},
+                                {"dataValue": {[Op.in]: partnerStations}}
+                            ]
+                        }
+
+                    }]
+                });
+
+                let result = [0, 0, 0, 0, 0, 0, 0]
+                for (let transaction of transactions.rows){
+                    let transactionDate = new Date(transaction['createdAt'])
+                    result[transactionDate.getDay()] = result[transactionDate.getDay()]+1
+                }
+
+                AnswerHttpRequest.done(res, result)
+            }
+            catch (error){
+                console.log(error)
+                AnswerHttpRequest.wrong(res, "Request failed")
+            }
+
+        }
     }
     catch (error){
         console.log(error)
-        AnswerHttpRequest.wrong(res, "Request failed")
-    }
-
-
-
-
-}),
-
-
-
-
-PartnerStatsRouter.get('/transaction/:stationPublicId/:operation/:from/:to', async (req, res) => {
-    let {stationPublicId, operation, from, to}  =req.params
-    try{
-
-        let station = await Station.findOne({where : {id: stationPublicId}})
-        if(station === null){
-            AnswerHttpRequest.wrong(res, "No station with the provided public Id")
-        }
-        else {
-            if(station.currentPartner !== req.body.partner.id){
-                AnswerHttpRequest.wrong(res, "You are not allowed to see stats for this station")
-            }
-            else {
-                try{
-                    let options = {
-                        attributes: [
-                            [fn('COUNT', col('id')), 'total']
-                        ],
-                        where : {
-                            operation: operation,
-                            createdAt: {
-                                [Op.between]: [new Date(from), new Date(to)],
-                            }}
-                    }
-                    let transactions  = await Transaction.findAndCountAll({
-                        where: {'operation': operation},
-                        include: [{
-                            model: TransactionMetaData,
-                            as: 'MetaData',
-                            where: {[Op.and]: [{"dataTitle": "stationId"}, {"dataValue": station.systemId.toString()}],}
-
-                        }]
-                    });
-                    AnswerHttpRequest.done(res, transactions.rows.length)
-                }
-                catch (error){
-                    console.log(error)
-                    AnswerHttpRequest.wrong(res, "Request failed")
-                }
-            }
-
-        }
-    }
-    catch (error){
         AnswerHttpRequest.wrong(res, "Request failed")
     }
 
