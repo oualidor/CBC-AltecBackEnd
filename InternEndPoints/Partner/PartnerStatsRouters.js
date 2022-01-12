3bcd320f2f7179b1459044555d3a4d0a49eafac5
oualidor <walid.khial@gmail.com>
2022-01-12 18:49:31 +0100

    Hi

--- InternEndPoints/Partner/PartnerStatsRouters.js
+++ InternEndPoints/Partner/PartnerStatsRouters.js
@@ -9,44 +9,81 @@ const {Op, fn, col} = require("sequelize");
 const  PartnerStatsRouter = express.Router()
 
 
-
-// PartnerStatsRouter.get('/transaction/:operation/:from/:to', async (req, res) => {
-//     let {operation, from, to}  =req.params
-//
-//
-//     try{
-//         let options = {
-//             attributes: [
-//                 [fn('COUNT', col('id')), 'total']
-//             ],
-//         }
-//         let transactions  = await Transaction.findAndCountAll({
-//             where: {
-//                 'operation': operation,
-//                 createdAt: {
-//                     [Op.between]: [new Date(from), new Date(to)],
-//                 }},
-//             include: [{
-//                 model: TransactionMetaData,
-//                 as: 'MetaData',
-//             }]
-//         });
-//         AnswerHttpRequest.done(res, transactions.rows.length)
-//     }
-//     catch (error){
-//         console.log(error)
-//         AnswerHttpRequest.wrong(res, "Request failed")
-//     }
-//
-//
-//
-//
-// }),
-
-
-
-
 PartnerStatsRouter.get('/transaction/:operation/:from/:to', async (req, res) => {
+    let {operation, from, to}  =req.params
+    try{
+        let partner = await Partner.findByPk(req.body.partner.id, {
+            include : [
+                {
+                    model: Station,
+                    as: "Stations"
+                }
+            ]
+        })
+        if(partner === null){
+            AnswerHttpRequest.wrong(res, "No stations affected to this partner, no stats")
+
+        }
+        else {
+            try{
+                let partnerStations = []
+                for (let station of partner['Stations']){
+                    partnerStations.push(station.systemId.toString())
+                }
+                let options = {
+                    attributes: [
+                        [fn('COUNT', col('id')), 'total']
+                    ],
+                    where : {
+                        operation: operation,
+                        createdAt: {
+                            [Op.between]: [new Date(from), new Date(to)],
+                        }}
+                }
+                let transactions  = await Transaction.findAndCountAll({
+                    raw: true,
+                    where: {'operation': operation},
+                    include: [{
+                        model: TransactionMetaData,
+                        as: 'MetaData',
+                        where: {
+                            [Op.and]: [
+                                {"dataTitle": "stationId"},
+                                {"dataValue": {[Op.in]: partnerStations}}
+                            ]
+                        }
+
+                    }]
+                });
+
+                // let result = [0, 0, 0, 0, 0, 0, 0]
+                // for (let transaction of transactions.rows){
+                //     let transactionDate = new Date(transaction['createdAt'])
+                //     result[transactionDate.getDay()] = result[transactionDate.getDay()]+1
+                // }
+
+                AnswerHttpRequest.done(res, transactions.count)
+            }
+            catch (error){
+                console.log(error)
+                AnswerHttpRequest.wrong(res, "Request failed")
+            }
+
+        }
+    }
+    catch (error){
+        console.log(error)
+        AnswerHttpRequest.wrong(res, "Request failed")
+    }
+
+}),
+
+
+
+
+
+
+PartnerStatsRouter.get('/transaction/:operation/:from/:to/:aa', async (req, res) => {
     let {operation, from, to}  =req.params
     try{
         let partner = await Partner.findByPk(req.body.partner.id, {
